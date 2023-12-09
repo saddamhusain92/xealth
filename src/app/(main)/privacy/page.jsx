42260d95c4e883cc125ac4f6ef7409df620afa34
saddam husain <saddamkhan.khan75@gmail.com>
2023-12-09 15:22:50 +0530

    page  add privacy

--- src/app/(main)/privacy/page.jsx
+++ src/app/(main)/privacy/page.jsx
@@ -2,45 +2,111 @@ import React from 'react'
 
 function Privacy() {
   return (
-    <div className='p-8'> 
-    <h1 className='text-4xl font-bold'>Privacy Policy</h1>
-    1. Information We Collect
-1.1 Personal Information:
-
-Account Information: To create and manage your account, we collect your email address, username, and password.
-Profile Information: You may choose to provide additional information, such as your name, profile picture, and other details to personalize your experience.
-1.2 Usage Information:
-
-Device Information: We collect information about your mobile device, including its unique identifier, operating system, and hardware details.
-Log Data: Our servers automatically record information about your usage of the application, including the features you use, the content you view, and any interactions with other users.
-1.3 Financial Information:
-
-Transaction Data: If the application involves financial transactions, we may collect information related to payments, such as billing details.
-2. How We Use Your Information
-2.1 Service Delivery:
-
-We use your personal information to create and manage your account, authenticate you, and provide the services offered by the Xwealth application.
-2.2 Communication:
-
-We may use your email address to send important updates, notifications, and information related to your account.
-2.3 Improving Services:
-
-Your usage information helps us analyze how our application is used, identify trends, and improve user experience and features.
-2.4 Security:
-
-We take measures to protect your information and ensure the security of our application.
-3. Sharing Your Information
-We do not sell, trade, or otherwise transfer your personal information to external parties without your consent, except as required by law.
-4. Data Security
-We implement industry-standard security measures to protect your information from unauthorized access, disclosure, or alteration.
-5. Your Choices and Rights
-You have the right to access, correct, or delete your personal information. You can manage your preferences within the application settings.
-6. Changes to this Privacy Policy
-We may update this Privacy Policy to reflect changes in our practices. Any material changes will be notified through the application or other means.
-7. Contact Us
-If you have any questions or concerns regarding this Privacy Policy, please contact us at [your contact email].
-
-By using the Xwealth mobile application, you acknowledge that you have read and understood this Privacy Policy.
+    <div className="ml-16 mr-16 mb-4">
+      <h1 className="text-4xl font-bold">Privacy Policy</h1>
+      <p className="text-md mt-2 font-bold">1. Information We Collect</p>
+      <p className="mt-2 text-sm font-bold">1.1 Personal Information:</p>
+      <ul class="list-disc">
+        <li>
+          <p className="mt-2 text-sm font-bold">Account Information:</p>
+          To create and manage your account, we collect your email address,
+          username, and password.
+        </li>
+        <li>
+          <p className="mt-2 text-sm font-bold">Profile Information: </p>
+          You may choose to provide additional information, such as your name,
+          profile picture, and other details to personalize your experience.
+        </li>
+      </ul>
+      <p className="mt-2 text-sm font-bold">1.2 Usage Information:</p>
+      <ul class="list-disc">
+        <li>
+          <p className="mt-2 text-sm font-bold">Device Information: </p>
+          We collect information about your mobile device, including its unique
+          identifier, operating system, and hardware details.
+        </li>
+        <li>
+          <p className="mt-2 text-sm font-bold">Log Data:</p>
+          Our servers automatically record information about your usage of the
+          application, including the features you use, the content you view, and
+          any interactions with other users.
+        </li>
+      </ul>
+      <p className="mt-2 text-sm font-bold">1.3 Financial Information:</p>
+      <ul class="list-disc">
+        <li>
+          <p className="mt-2 text-sm font-bold">Transaction Data:</p>
+          If the application involves financial transactions, we may collect
+          information related to payments, such as billing details.
+        </li>
+      </ul>
+      <p className="text-md mt-2 font-bold">2. How We Use Your Information</p>
+      <p className="mt-2 text-sm font-bold">2.1 Service Delivery:</p>
+      <ul class="list-disc">
+        <li>
+          We use your personal information to create and manage your account,
+          authenticate you, and provide the services offered by the Xwealth
+          application.
+        </li>
+      </ul>
+      <p className="mt-2 text-sm font-bold">2.2 Communication:</p>
+      <ul class="list-disc">
+        <li>
+          We may use your email address to send important updates,
+          notifications, and information related to your account.
+        </li>
+      </ul>
+      <p className="mt-2 text-sm font-bold">2.3 Improving Services:</p>
+      <ul class="list-disc">
+        <li>
+          Your usage information helps us analyze how our application is used,
+          identify trends, and improve user experience and features.
+        </li>
+      </ul>
+      <p className="mt-2 text-sm font-bold">2.4 Security:</p>
+      <ul class="list-disc">
+        <li>
+          We take measures to protect your information and ensure the security
+          of our application.
+        </li>
+      </ul>
+      <p className="text-md mt-2 font-bold">3. Sharing Your Information</p>
+      <ul class="list-disc">
+        <li>
+          We do not sell, trade, or otherwise transfer your personal information
+          to external parties without your consent, except as required by law.
+        </li>
+      </ul>
+      <p className="text-md mt-2 font-bold">4. Data Security</p>
+      <ul class="list-disc">
+        <li>
+          We implement industry-standard security measures to protect your
+          information from unauthorized access, disclosure, or alteration.
+        </li>
+      </ul>
+      <p className="text-md mt-2 font-bold">5. Your Choices and Rights</p>
+      <ul class="list-disc">
+        <li>
+          You have the right to access, correct, or delete your personal
+          information. You can manage your preferences within the application
+          settings.
+        </li>
+      </ul>
+      <p className="text-md mt-2 font-bold">
+        6. Changes to this Privacy Policy
+      </p>
+      <ul class="list-disc">
+        <li>
+          We may update this Privacy Policy to reflect changes in our practices.
+          Any material changes will be notified through the application or other
+          means.
+        </li>
+      </ul>
+      <p className="text-md mt-2 font-bold">7. Contact Us</p>
+      If you have any questions or concerns regarding this Privacy Policy,
+      please contact us at [your contact email]. By using the Xwealth mobile
+      application, you acknowledge that you have read and understood this
+      Privacy Policy.
     </div>
   )
 }
